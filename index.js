ceb88bb1baf62cc025f9e910ac8d00172b745ba6
Alfonso Graziano <alfonso.graziano@nearform.com>
2023-10-20 11:11:40 +0200

    fix: porting to cjs (#72)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import fp from 'fastify-plugin'
-import mercurius from 'mercurius'
+const fp = require('fastify-plugin')
+const mercurius = require('mercurius')
 
 const PLUGIN_NAME = 'mercuriusDynamicSchema'
 const STRATEGY_NAME = 'mercuriusDynamicSchemaStrategy'
@@ -24,59 +24,61 @@ function strategyFactory({ name, strategy }) {
   }
 }
 
-const plugin = fp(
-  async function (fastify, opts) {
-    const constraintStrategy = strategyFactory({
-      name: STRATEGY_NAME,
-      strategy: opts.strategy
-    })
+async function mercuriusDynamicSchema(fastify, opts) {
+  const constraintStrategy = strategyFactory({
+    name: STRATEGY_NAME,
+    strategy: opts.strategy
+  })
 
-    fastify.addConstraintStrategy(constraintStrategy)
+  fastify.addConstraintStrategy(constraintStrategy)
 
-    const contextFn = opts.context
+  const contextFn = opts.context
 
-    for (const schema of opts.schemas) {
-      await fastify.register(
-        async childServer => {
-          childServer.register(mercurius, {
-            schema: schema.schema,
-            resolvers: schema.resolvers,
-            graphiql: false,
-            routes: false
-          })
+  for (const schema of opts.schemas) {
+    await fastify.register(
+      async childServer => {
+        childServer.register(mercurius, {
+          schema: schema.schema,
+          resolvers: schema.resolvers,
+          graphiql: false,
+          routes: false
+        })
 
-          childServer.route({
-            path: schema?.path ?? '/graphql',
-            method: 'POST',
-            constraints: { [STRATEGY_NAME]: schema.name },
-            handler: async (req, reply) => {
-              let { query, operationName, variables } = req.body
+        childServer.route({
+          path: schema?.path ?? '/graphql',
+          method: 'POST',
+          constraints: { [STRATEGY_NAME]: schema.name },
+          handler: async (req, reply) => {
+            let { query, operationName, variables } = req.body
 
-              if (typeof req.body === 'string') {
-                query = req.body
-              }
-
-              if (contextFn) {
-                req[kRequestContext] = await contextFn(req, reply)
-                Object.assign(req[kRequestContext], { reply, childServer })
-              } else {
-                req[kRequestContext] = { reply, app: childServer }
-              }
-
-              return reply.graphql(
-                query,
-                req[kRequestContext],
-                variables,
-                operationName
-              )
+            if (typeof req.body === 'string') {
+              query = req.body
             }
-          })
-        },
-        { name: `${PLUGIN_NAME}.${schema.name}` }
-      )
-    }
-  },
-  { fastify: '4.x', name: PLUGIN_NAME }
-)
 
-export default plugin
+            if (contextFn) {
+              req[kRequestContext] = await contextFn(req, reply)
+              Object.assign(req[kRequestContext], { reply, childServer })
+            } else {
+              req[kRequestContext] = { reply, app: childServer }
+            }
+
+            return reply.graphql(
+              query,
+              req[kRequestContext],
+              variables,
+              operationName
+            )
+          }
+        })
+      },
+      { name: `${PLUGIN_NAME}.${schema.name}` }
+    )
+  }
+}
+
+module.exports = fp(mercuriusDynamicSchema, {
+  fastify: '4.x',
+  name: PLUGIN_NAME
+})
+module.exports.default = mercuriusDynamicSchema
+module.exports.mercuriusDynamicSchema = mercuriusDynamicSchema
